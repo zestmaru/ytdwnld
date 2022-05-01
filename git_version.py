aba5d7017459327de9d4f36db6fdcc2eff4c2cae
zestmaru <kamekaddze@gmail.com>
2022-05-02 00:41:06 +0300

    gh action develop workflow

--- git_version.py
+++ git_version.py
@@ -1,8 +1,17 @@
 import git
 
 repo = git.Repo(search_parent_directories=True)
+
+# get hash
 sha = repo.head.object.hexsha
+shortSha = sha[:10] # first 10 chars
 
-shortSha = sha[:10]
+# get all commits reachable from "HEAD"
+commits = list(repo.iter_commits('HEAD'))
 
-print(shortSha)
+# get the number of commits
+countCommits = len(commits)
+
+devVersion = "r{}~{}".format(countCommits, shortSha)
+
+print(devVersion) #r<number of commits>~<shorthash of commit>
